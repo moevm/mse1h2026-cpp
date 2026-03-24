80cb3432a0dd62dc3260c9c3731c122f115ab0e2
‪Mahmoud Mohamed‬‏ <125126600+saicooo@users.noreply.github.com>
2026-03-24 18:38:38 +0300

    Add files via upload

---
+++ workshop/src/c++_course/base_module/__init__.py
@@ -0,0 +1,2 @@
+from src.c_course.base_module.base_task import BaseTaskClass, TestItem  # noqa: F401
+from src.c_course.base_module.base_cli import add_common_cli_args, get_common_cli_args, CLIParser  # noqa: F401
