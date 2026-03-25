21a2fd6848a3611d7a8ee3ce502e8161a6e5b776
faroukbelaid09 <faroukbelaid9@gmail.com>
2026-03-25 15:08:00 +0300

    Resolve merge conflicts (cpp_course + operators_overloading)

--- workshop/src/cpp_course/operators_overloading/__init__.py
+++ workshop/src/cpp_course/operators_overloading/__init__.py
@@ -1,2 +1,11 @@
-from .operators_overloading_test import OperatorsOverloadingTest
-from .operators_overloading_cli import operators_overloading_parser
+from .operators_overloading_test import (
+    OperatorsOverloadingEasy1Test,
+    OperatorsOverloadingMid1Test,
+    OperatorsOverloadingMid2Test,
+)
+
+from .operators_overloading_cli import (
+    easy_task_1_parser,
+    mid_task_1_parser,
+    mid_task_2_parser,
+)
